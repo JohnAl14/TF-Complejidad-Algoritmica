e29c971a29607cf56e71bedc03ae2973570e558d
JohnAl14 <134559578+JohnAl14@users.noreply.github.com>
2023-11-23 09:23:19 -0500

    Delete AlgComplexTF/TF/directorios.py
    Archivo innecesario

--- AlgComplexTF/TF/directorios.py
+++
@@ -1,8 +0,0 @@
-import os; import pandas as pd
-
-ruta_carpeta = 'C:\\Users\\USER\\Desktop\\Archivos\\AlgComplexTF\\archivos'
-ruta_completa1 = 'C:\\Users\\USER\\Desktop\\Archivos\\AlgComplexTF\\archivos'
-nombre_archivo = 'archivo_coordenadas.csv'
-nombre_archivo1 = 'archivo_aristas.csv'
-ruta_completa = os.path.join(ruta_carpeta, nombre_archivo)
-ruta_completa1 = os.path.join(ruta_carpeta, nombre_archivo1)
